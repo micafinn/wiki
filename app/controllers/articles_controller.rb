0a9ff484579fb382873720cae5b4667e9ef51d16
micafinn <micafinn@gmail.com>
2017-04-05 15:47:01 +0200

    Add ability to arrange/show articles by category

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,12 @@ class ArticlesController < ApplicationController
   before_action :find_article, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    @articles = Article.all.order("created_at DESC")
+    if params[:category].blank?
+      @articles = Article.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @articles = Article.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
   def show
