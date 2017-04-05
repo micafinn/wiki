a3c4cd2b9c6a8bcbcc33c7fc16b0a49cc7102cb6
micafinn <micafinn@gmail.com>
2017-04-05 15:15:35 +0200

    Create category models and corresponding views and associations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :content)
+    params.require(:article).permit(:title, :content, :category_id)
   end
 
 end
