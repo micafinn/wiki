e0cde9c63fff4731ce39ef92c6c0b917a9db850e
micafinn <micafinn@gmail.com>
2017-04-05 14:45:22 +0200

    Associate articles with users and add authentication requirements

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :find_article, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @articles = Article.all.order("created_at DESC")
   end
@@ -9,11 +10,11 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    @article = Article.new
+    @article = current_user.articles.build
   end
 
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
     if @article.save
       redirect_to @article
     else
